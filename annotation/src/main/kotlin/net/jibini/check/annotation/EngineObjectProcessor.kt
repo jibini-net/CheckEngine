f7590d9ec1ef63a3a0c0d030ca0780eea3534e6a
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2020-10-19 19:45:27 -0500

    EngineObjectProcessor scans and caches annotated classes

--- annotation/src/main/kotlin/net/jibini/check/annotation/EngineObjectProcessor.kt
+++ annotation/src/main/kotlin/net/jibini/check/annotation/EngineObjectProcessor.kt
@@ -1,43 +1,62 @@
 package net.jibini.check.annotation
 
+import java.io.PrintWriter
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.TypeElement
-import javax.lang.model.util.ElementFilter
+import javax.lang.model.element.*
+import javax.tools.StandardLocation
 
 @SupportedAnnotationTypes("net.jibini.check.annotation.EngineObject")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 class EngineObjectProcessor : AbstractProcessor()
 {
-    private var alreadyDone = false
+    private var writer: PrintWriter? = null
+    private val writtenClasses = mutableListOf<String>()
 
     override fun getSupportedAnnotationTypes(): MutableSet<String> = mutableSetOf(EngineObject::class.java.name)
 
     override fun process(annotations: MutableSet<out TypeElement>, roundEnv: RoundEnvironment): Boolean
     {
-        if (alreadyDone)
-            return false
-        alreadyDone = true
-
         val annotated = roundEnv.getElementsAnnotatedWith(EngineObject::class.java)
-        val types = ElementFilter.typesIn(annotated)
 
-        val fileObject = processingEnv.filer.createSourceFile("net.jibini.check.generated.EngineObjects")
-        val writer = fileObject.openWriter()
+        if (writer == null)
+        {
+            val probe = processingEnv.filer.createResource(StandardLocation.CLASS_OUTPUT,
+                "", "probe.tmp")
 
-        writer.write("""
-            package net.jibini.check.generated;
-            
-            public class EngineObjects
-            {
-                public static void initialize()
+            val file = processingEnv.filer.createResource(StandardLocation.CLASS_OUTPUT, "",
+                when
                 {
-                    
-                }
-            }
-        """.trimIndent())
+                    probe.toUri().path.contains("kapt3/classes/test") -> "engine_scan_test.txt"
 
-        writer.close()
+                    else -> "engine_scan.txt"
+                }
+            )
+
+            probe.delete()
+
+            writer = PrintWriter(file.openWriter())
+        }
+
+        val names = mutableListOf<String>()
+
+        for (item in annotated)
+            when
+            {
+                item.kind.isField -> names += (item.enclosingElement as TypeElement).qualifiedName.toString()
+
+                item.kind.isClass -> names += (item as TypeElement).qualifiedName.toString()
+            }
+
+        for (name in names)
+            if (!writtenClasses.contains(name))
+            {
+                writer!!.println(name)
+
+                writtenClasses += name
+            }
+
+        writer!!.flush()
 
         return false
     }
